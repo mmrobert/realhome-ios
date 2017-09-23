5d8e51fd1f1fac24993fe4d8fdac966fc08a6d47
semitalk <semitalk.inc@gmail.com>
2017-09-23 12:38:40 -0400

    Initial Commit

---
+++ RealHome/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RealHome
+//
+//  Created by boqian cheng on 2017-09-23.
+//  Copyright © 2017 boqiancheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
